20941b8f188acaa4252c7fe5c1605024e25d48d6
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-01-02 18:25:55 +0200

    formEdit

--- src/Components/Contact/Form.jsx
+++ src/Components/Contact/Form.jsx
@@ -2,40 +2,70 @@ import React, { useState } from "react";
 import Style from "./Contact.module.scss";
 
 export default function Form() {
-    // const [placeHolder, setPlaceHolder] = useState("");
-    // let [addShow, setaddShow] = useState(false);
-    // function show(val , ph){
-    //     if(val){
-    //         setaddShow(true);
-    //         setPlaceHolder(`${ph} :`);
-    //     } else{
-    //         setaddShow(false);
-    //     }
-    //     return addShow;
-    // }
-  
-
+  function showSpanElement(input) {
+    if (input.value) {
+      input.previousElementSibling.classList.replace("hide", "show");
+      input.previousElementSibling.innerHTML = "";
+      input.previousElementSibling.append(input.placeholder);
+    } else {
+      input.previousElementSibling.classList.replace("show", "hide");
+    }
+  }
 
   return (
     <>
       <form action="">
         <div className={`${Style["input-box"]}`}>
-            <span className={``}>{}</span>
-            <input  autoComplete="current-userName" className="my-3" type="text" placeholder="userName"/>
+          <span className={`hide`}></span>
+          <input
+            onInput={function (e) {
+              showSpanElement(e.target);
+            }}
+            autoComplete="current-userName"
+            className="my-3"
+            type="text"
+            placeholder="userName"
+          />
         </div>
         <div className={`${Style["input-box"]}`}>
-            <span className={``}>{}</span>
-            <input  className="my-3" autoComplete="current-age"  type="text" placeholder="userAge"/>
+          <span className={`hide`}>{}</span>
+          <input
+            onInput={function (e) {
+              showSpanElement(e.target);
+            }}
+            className="my-4"
+            autoComplete="current-age"
+            type="text"
+            placeholder="userAge"
+          />
         </div>
         <div className={`${Style["input-box"]}`}>
-            <span className={``}>{}</span>
-            <input  className="my-3" autoComplete="current-email" type="email" placeholder="userEmail"/>
+          <span className={`hide`}>{}</span>
+          <input
+            onInput={function (e) {
+              showSpanElement(e.target);
+            }}
+            className="my-4"
+            autoComplete="current-email"
+            type="email"
+            placeholder="userEmail"
+          />
         </div>
         <div className={`${Style["input-box"]}`}>
-            <span className={``}>{}</span>
-            <input  className="my-3" autoComplete="current-password" type="password" placeholder="userPassword"/>
+          <span className={`hide`}>{}</span>
+          <input
+            onInput={function (e) {
+              showSpanElement(e.target);
+            }}
+            className="my-4"
+            autoComplete="current-password"
+            type="password"
+            placeholder="userPassword"
+          />
         </div>
-        <button type="submit" className={`${Style["send-btn"]}`}>Send Message</button>
+        <button type="submit" className={`${Style["send-btn"]}`}>
+          Send Message
+        </button>
       </form>
     </>
   );
